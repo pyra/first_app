e46ec7bd572b4dd021b64b9e60852c255fc9a135
pyra <pyronek@wp.pl>
2013-09-19 22:40:47 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ForstApp::Application.config.secret_key_base = 'c81e42098ca369ebd73aa44a76976b66f79fffde0254e5520c52fcfa3fa4605da130182a5453bada8b1eed10d830db501f293dee2a90caee20df29ffa3bc38ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ForstApp::Application.config.session_store :cookie_store, key: '_forst_app_session'
